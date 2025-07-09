5bc834a645a3f7ee4b2fbeb7b1a11bd298a43c2f
gojodept <arindamkashyap625@gmail.com>
2025-07-09 15:45:43 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Home from './components/Home.jsx'
+
+export default function App() {
+  return <Home name="John" age={21}/>
+  }
+ 
---
+++ src/components/Home.jsx
@@ -0,0 +1,10 @@
+export default function Home({name,age}){
+  let id=12213427
+  return (
+    <div>
+    <h1>Hello {name}.You are {age} years old</h1>
+    <p>This is the id {id}</p>
+    </div>
+    );
+    //is parent element and one parent element is compulsory 
+}
