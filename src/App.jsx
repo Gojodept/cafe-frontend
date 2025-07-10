f66e6b57b2056f70e97196684c8367bddff1d952
gojodept <arindamkashyap625@gmail.com>
2025-07-11 03:43:09 +0530

    Login

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Register from './components/Register'
+import Login from './components/Login'
 // import Home from './components/Home.jsx'
 // import Temp from './components/Temp.jsx'
 // export default function App() {
@@ -18,6 +19,7 @@ import Register from './components/Register'
     <div>
       <h1>Cafe Frontend</h1>
       <Register/>
+      <Login/>
       <h3>This is the footer</h3>
     </div>
   )
---
+++ src/components/Login.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { useState } from "react";
+import axios from "axios";
+export default function Login() {
+  const [user, setUser] = useState({});
+  const [error, setErrror] = useState();
+  const API_URL = import.meta.env.VITE_API_URL;
+  const handleSubmit = async () => {
+    console.log(user);
+    try {
+      //const url = "http://localhost:8080/api/users/login";
+      const url = `${API_URL}/api/users/login`;
+      const result = await axios.post(url, user); // ✅ send user object
+      setErrror("✅ Login Success");
+    } catch (err) {
+      console.error(err);
+      setErrror("Something went wrong.");
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login form </h2>
+      <p>
+        <input
+          type="text"
+          placeholder="Email"
+          onChange={(e) => setUser({ ...user, email: e.target.value })}
+        />
+      </p>
+      <p>
+        <input
+          type="password"
+          placeholder="Password"
+          onChange={(e) => setUser({ ...user, password: e.target.value })}
+        />
+      </p>
+      <p>
+        <button onClick={handleSubmit}>Submit</button>
+      </p>
+      {error && <p>{error}</p>}
+    </div>
+  );
+}
